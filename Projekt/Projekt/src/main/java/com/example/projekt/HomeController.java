677d3ca1c567584014a0375464a7e9f0de3ce865
Adrian Włodarczyk <33695278+Shade644@users.noreply.github.com>
2022-06-15 19:10:24 +0200

    Wyświetlanie danych z bazy na stronie

--- Projekt/Projekt/src/main/java/com/example/projekt/HomeController.java
+++ Projekt/Projekt/src/main/java/com/example/projekt/HomeController.java
@@ -3,13 +3,25 @@ package com.example.projekt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 @Autowired
 FilmRepo repo;
+
+@GetMapping("/")
+    public ModelAndView showData(){
+        ModelAndView mv =new ModelAndView("Film");
+
+        //Iterable<Film> datalist =repo.findAll("Film");
+        mv.addObject("test",repo.findAll());
+        mv.setViewName("glowna.html");
+
+        return mv;
+    }
+
     @RequestMapping("/Strona")
     public String Strona(){
         return "Strona.html";
